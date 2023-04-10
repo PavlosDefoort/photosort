eccf01fe0951a671de074ee16ed78941bec5a85c
PavlosDefoort <psnpenguin2@gmail.com>
2023-04-10 19:45:36 -0400

    Testing

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    customKey: process.env.API_KEY,
-    auth: process.env.AUTH,
-    userName: process.env.USERNAME,
-    password: process.env.PASSWORD,
-  },
+  env: { customKey: process.env.API_KEY },
 
   reactStrictMode: true,
 };
--- src/pages/postman.js
+++ src/pages/postman.js
@@ -13,7 +13,6 @@ import MultipleCheckTemplate from "./MultiSelect";
 
 import GridGallery from "./offthegrid";
 import { Disclosure } from "@headlessui/react";
-import Generate from ".//api/generateToken";
 
 function HomePage() {
   const [posts, setPosts] = useState([]);
@@ -32,46 +31,9 @@ function HomePage() {
   const [time, setTime] = useState("all");
   const [isTime, setIsTime] = useState(false);
   const [nsfw, setNsfw] = useState(false);
-  const [token, setToken] = useState(null);
+  const daKey = process.env.customKey;
 
-  useEffect(() => {
-    if (!token) {
-      setToken(Generate());
-    }
-  }, [token]);
-
-  const bearer = "bearer " + token?.access_token ?? "bearer";
-
-  console.log(token);
-
-  function Generate() {
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Basic " + process.env.auth);
-    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-    myHeaders.append(
-      "Cookie",
-      "csv=2; edgebucket=GNBcA2zVmNgEdkIhDc; loid=0000000000adnpt678.2.1613408283000.Z0FBQUFBQmtOR20tQTVuS0NJaUFpWlV5LUtqZDdwSXU0QkNKU3Rialp0NlR5RUdpRGZoNVZXbzJGN2hBZzhyRkpramxISjRIYlM0VTIwYXM2QVlkV2g3cllfXzFZTHFSTXdyOWFLcUY0WjBCLU9kcWhnOEZOSnFJUXhaa3dnXzQxRlZnTFRKb2oyS1Q; session_tracker=bedprdqokhnbrlkodc.0.1681156708161.Z0FBQUFBQmtOR3BrdWh5WDhjS3V5SThQcTJhOTEtZjNQdHUwamtmM0ltRzFTOG9EZFUtWHVJZFdVY3BQcEQ0c1hRVUxyUXA4Qy0tXzBqdDAwZjQxQjN0T1lwc2g0MWx6R2tnQXJ6Z2dkV1FCQXgzQWVLbHVSTTVLXzFNMVVyVFJaT2xSNXlmMC1ucFg"
-    );
-
-    var urlencoded = new URLSearchParams();
-    urlencoded.append("grant_type", "password");
-    urlencoded.append("username", process.env.userName);
-    urlencoded.append("password", process.env.password);
-
-    var requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: urlencoded,
-      redirect: "follow",
-    };
-
-    fetch("https://www.reddit.com/api/v1/access_token", requestOptions)
-      .then((response) => response.text())
-      .then((result) => {
-        return result;
-      })
-      .catch((error) => console.log("error", error));
-  }
+  const bearer = "bearer " + daKey;
 
   useEffect(() => {
     if (posts.length >= max || count == 10) {
@@ -107,7 +69,7 @@ function HomePage() {
     }
 
     fetch(url, requestOptions)
-      .then((response) => response.json()) // parse response as JSON (can be res.text() for plain response)
+      .then((response) => response.json()) // parse response as JSON
       .then((result) => {
         if (result != null) {
           const filteredArticles = result.data.children.filter((post) => {
@@ -239,7 +201,6 @@ function HomePage() {
               <MultipleSelectCheckmarks
                 props={subreddit}
                 onSelectChange={handleSelectChange}
-                bearer={bearer}
               />
               <div style={{ padding: "1rem" }}>
                 <input
